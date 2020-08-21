2437ccaefac2896fc62a3c5003600846b51c5f67
Lens Mutombo <lensmutombo@gmail.com>
2020-08-21 22:23:18 +0200

    Update Form2.Designer.cs

--- Csharp2MongoDB/Csharp2MongoDB/Form2.Designer.cs
+++ Csharp2MongoDB/Csharp2MongoDB/Form2.Designer.cs
@@ -32,10 +32,13 @@
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(43)))), ((int)(((byte)(69)))));
-            this.ClientSize = new System.Drawing.Size(1260, 511);
+            this.ClientSize = new System.Drawing.Size(1227, 722);
+            this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form2";
             this.Text = "Form2";
             this.ResumeLayout(false);
