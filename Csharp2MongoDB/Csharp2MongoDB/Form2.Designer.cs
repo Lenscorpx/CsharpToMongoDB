488845a982263e4e92560a5897b75ad65e8cc73b
Lens Mutombo <lensmutombo@gmail.com>
2020-08-22 23:39:32 +0200

    Update Form2.Designer.cs

--- Csharp2MongoDB/Csharp2MongoDB/Form2.Designer.cs
+++ Csharp2MongoDB/Csharp2MongoDB/Form2.Designer.cs
@@ -33,9 +33,10 @@
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(353, 174);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.Size = new System.Drawing.Size(1227, 158);
             this.panel1.TabIndex = 0;
             // 
             // Form2
